16597661ad9262ec394b7d3558f95a9f6df6f7b5
Adam <yemengtao1986@hotmail.com>
2019-05-23 13:50:02 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,5 +1,6 @@
 let member = [];
-for(var i=1;i<251;i++){
+member = []
+for(var i=1;i<316;i++){
+  if(i.toString()[i.toString().length-1]!=='4' && i.toString()[i.toString().length-2]!=='4') --剔除个位及十位数含4的数字
   member.push({"phone":"号",'name':i})
 }
-console.log(member)
